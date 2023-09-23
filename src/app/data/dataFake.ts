f91f1fff9607a1f8237f885feae7407c8cdd8ce1
Matheus-Marthek <matheusmarthek@gmail.com>
2023-09-23 18:00:24 -0300

    adding responsive content and fix menu bar issue

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photo":"../../../assets/imagens/print-pokedex-1.png",
+        "title":"Novo projeto em angular [.blog]",
+        "description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eius labore, consequuntur dignissimos id maxime mollitia. Vitae, impedit, illum dignissimos tempora animi, at quibusdam provident sunt labore ducimus suscipit quaerat!"
+    },
+    {
+        "id":"2",
+        "photo":"aaaaaa",
+        "title":"blog 2",
+        "description":"lorem ipsuam hsuahsau"
+    }
+]
